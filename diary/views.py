85d0337606210ccbf136a634f732b4bb00674998
masashi.takada <b132036@gmail.com>
2021-11-23 04:24:14 +0900

    20211123 フォーム画面の作成

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from . import forms
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = forms.InquiryForm
