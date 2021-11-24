0dca2216f18442e69a7e0a4d5c9ccd1c9785f102
masashi.takada <b132036@gmail.com>
2021-11-25 05:24:10 +0900

    settings分割

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -14,16 +14,18 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j$de^w%w@6!0+t-b-7(re_fw4zb=ge9*+6ic1-v-f6t!ey)ky='
-
 
 # Application definition
 
